0d51ebf16e502600a46e3eb82a4222c008eed1bb
samacriano mendes <samacriano@gmail.com>
2020-02-10 13:24:21 +0000

    mensagem para rodrigo

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Rodrigo nao trabalha nos meus files para nao haver conflict
         </p>
         <a
           className="App-link"
