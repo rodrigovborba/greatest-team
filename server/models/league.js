49df03ae9ed20537f593ee93e6a41df297cc5624
Alexis Le Forestier <55847981+another-dj@users.noreply.github.com>
2020-02-11 17:12:47 +0000

    Create league.js

---
+++ server/models/league.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true
+  },
+  seasons: [
+    { type : mongoose.Schema.Types.ObjectId, ref: 'Season' }
+  ],
+  description: {
+    type: String,
+    trim: true
+  },
+  teams: [
+    { type : mongoose.Schema.Types.ObjectId, ref: 'Team' }
+  ],
+  classifications: [
+    { type : mongoose.Schema.Types.ObjectId, ref: 'Classification' }
+  ],
+  country: {
+    type: mongoose.Schema.Types.ObjectId, ref: 'Country'
+  },
+  continent: {
+    type: mongoose.Schema.Types.ObjectId, ref: 'Continent'
+  }
+});
+
+module.exports = mongoose.model('League', schema);
